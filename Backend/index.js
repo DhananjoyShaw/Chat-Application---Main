05ced1ce29c2b6bf3baea0a11d93f6b03a555892
DhananjoyShaw <dhananjoyshaw0218@gmail.com>
2025-03-20 16:46:08 +0530

    deployment

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-application-frontend-jtzm.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
