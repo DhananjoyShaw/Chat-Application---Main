894230671786b4416c4511a31eb873a1e652472d
DhananjoyShaw <dhananjoyshaw0218@gmail.com>
2025-03-20 21:12:31 +0530

    deployment

--- Frontend/src/url.js
+++ Frontend/src/url.js
@@ -1,2 +1,3 @@
 // export const URL = "http://localhost:8000";
-export const URL = "https://chat-application-backend-6q5i.onrender.com";
+
+export const URL = "https://chat-application-backend-6h4e.onrender.com";
